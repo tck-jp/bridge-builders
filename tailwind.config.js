0706290c7451b0395f3611186fe43a33c8e5480b
devinvm <devinvm@icloud.com>
2026-03-15 19:35:03 +0900

    Add Favicon, new text styling, update homepage content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
   darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
