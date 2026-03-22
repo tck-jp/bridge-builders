ebac1b0a1007928312d71402850fa491562947bc
devinvm <devinvm@icloud.com>
2026-03-22 12:51:42 +0900

    SEO improvements, fixed member name in JP

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: "https://www.tckbridgebuilders.com",
   adapter: vercel(),
   i18n: {
     defaultLocale: "en",
